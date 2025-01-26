94d156dacc1b5981a16c151aebf9e6b158f0ebaf
Bahadir Cambel <bcambel@gmail.com>
2025-01-26 15:33:59 +0100

    comptime for gametime

--- exercises/072_comptime7.zig
+++ exercises/072_comptime7.zig
@@ -13,7 +13,8 @@
 //
 // You haven't taken off that wizard hat yet, have you?
 //
-const print = @import("std").debug.print;
+const std = @import("std");
+const print = std.debug.print;
 
 pub fn main() void {
     // Here is a string containing a series of arithmetic
@@ -35,11 +36,13 @@ pub fn main() void {
     // at compile time.
     //
     // Please fix this to loop once per "instruction":
-    ??? (i < instructions.len) : (???) {
+    inline while (i < instructions.len) : (i += 3) {
 
         // This gets the digit from the "instruction". Can you
         // figure out why we subtract '0' from it?
         const digit = instructions[i + 1] - '0';
+        // print("Mold {}|{c}\n", .{ instructions[i + 1], instructions[i + 1] });
+        // print("Zer0 is {d}\n", .{'0'});
 
         // This 'switch' statement contains the actual work done
         // at runtime. At first, this doesn't seem exciting...
@@ -47,7 +50,7 @@ pub fn main() void {
             '+' => value += digit,
             '-' => value -= digit,
             '*' => value *= digit,
-            else => unreachable,
+            else => unreachable, //print("Hold {}", .{instructions[i + 1]}),
         }
         // ...But it's quite a bit more exciting than it first appears.
         // The 'inline while' no longer exists at runtime and neither
